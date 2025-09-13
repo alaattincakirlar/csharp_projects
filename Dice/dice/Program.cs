bdd4c8e511205ec0081cf15f9533ea1224ed2c4c
alaattincakirlar <mr.cakirlar@hotmail.com>
2025-09-13 03:23:54 +0300

    Done.

---
+++ Dice/dice/Program.cs
@@ -0,0 +1,27 @@
+﻿Random dice = new Random();
+
+int i = 0, j = 0;
+int summary = 0;
+int dice_value = 0;
+
+for (i = 0; i < 2; i++)
+{
+    dice_value = dice.Next(1, 7);
+
+    if (dice_value == summary)
+    {
+        j++;
+    }
+
+    summary = summary + dice_value;
+}
+
+if (j == 1)
+{
+    Console.WriteLine($"Dice roll of two: {summary} and **BINGO**");
+}
+
+else
+{
+    Console.WriteLine($"Dice roll of two: {summary}");
+}
