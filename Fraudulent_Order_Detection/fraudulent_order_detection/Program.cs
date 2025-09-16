aa1a7ddb515de79195d7d4f189509694a86096f5
alaattincakirlar <mr.cakirlar@hotmail.com>
2025-09-16 21:59:01 +0300

    Yeni proje eklendi. (2)

---
+++ Fraudulent_Order_Detection/fraudulent_order_detection/Program.cs
@@ -0,0 +1,10 @@
+﻿string[] orders = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"]; //string array oluşturma
+
+foreach (string order in orders) //oluşturduğumuz array'i kullanarak döngüye sokma
+{
+    if (order.StartsWith("B")) //oluşturulan string array içerisinde "B" ile başlayan eleman tespiti
+    {
+        Console.WriteLine(order);
+    }
+}
+
